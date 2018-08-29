d0adc05a5b2cae3ec3acec50bf24e5067cc371ac
kylo-ben11 <ben.knier@gmail.com>
2018-08-29 09:38:08 -0500

    initial commit and style parallax

---
+++ wp-content/themes/shapely-child/gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+// Requires the gulp-sass plugin
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+  return gulp.src('source-files')
+    .pipe(sass()) // Using gulp-sass
+    .pipe(gulp.dest('destination'))
+});
+
+gulp.task('sass', function(){
+  return gulp.src('shapely-child/scss/styles.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('shapely-child/css'))
+});
---
+++ wp-content/themes/shapely/template-parts/content-kylo-ren.php
@@ -0,0 +1,375 @@
+<style media="screen">
+html,body,div,span,applet,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video{font-size:100%;font:inherit;padding:0;border:0;margin:0;vertical-align:baseline}body{line-height:1}ol,ul{list-style:none}blockquote,q{quotes:none}blockquote:before,blockquote:after,q:before,q:after{content:'';content:none}table{border-collapse:collapse;border-spacing:0}article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section{display:block}.clear{clear:both}
+.sticky{}.bypostauthor{}.wp-caption{}.wp-caption-text{}.gallery-caption{}.alignright{}.alignleft{}.aligncenter{}
+
+textarea:focus, input:focus{outline: none;}
+*:focus {outline: none;}
+
+body {
+	width: 100%;
+	font-family: Arial;
+	-webkit-font-smoothing: antialiased;
+	line-height: 1.1;
+  background-color: #131313;
+}
+
+
+h1,h2,h3,h4,h5,h6 {
+	margin: 0px;
+	font-family: 'Open Sans', sans-serif;
+	font-weight: 300;
+	font-style: normal;
+	line-height: 1.3;
+}
+
+h1 {font-size: 30px;}
+h2 {font-size: 22px;}
+h3 {font-size: 18px;}
+h4 {font-size: 16px;}
+h5 {font-size: 14px;}
+h6 {font-size: 12px;}
+
+.normal {font-weight: 400;}
+.semibold {font-weight: 600;}
+.bold {font-weight: 700;}
+
+p {
+	font-family: 'Open Sans', sans-serif;
+	font-weight: 300;
+	line-height: 1.3;
+	font-size: 16px;
+}
+
+b, strong {font-weight: bold;}
+i {font-style: italic;}
+a {
+	text-decoration: none;
+	color: inherit;
+}
+
+input {-webkit-appearance: none;}
+
+
+.wrapper {
+	width: 100%;
+	float: left;
+}
+
+.full-width {
+	width: 100%;
+	float: left;
+	box-sizing: border-box;
+}
+.fixed-width {
+	width: 940px;
+	margin: 0 auto;
+	box-sizing: border-box;
+}
+
+
+
+
+.explore {
+	width: 700px;
+	padding: 0 0 30px 0;
+	margin: 0 auto;
+	text-align: center;
+  clear: both;
+}
+.explore h2 {
+	margin: 10px 0;
+	font-size: 24px;
+	color: #F01E39;
+}
+.explore button {
+	color: #F01E39;
+	border: 1px solid #F01E39;
+	padding: 5px 10px;
+	background: none;
+	cursor: pointer;
+	font-family: 'Open Sans', sans-serif;
+	font-weight: 300;
+	font-size: 16px;
+	-moz-transition: .2s ease-in-out;
+  -o-transition: .2s ease-in-out;
+  -webkit-transition: .2s ease-in-out;
+  transition: .2s ease-in-out;
+}
+.explore button:hover,
+.explore button.active {
+	background: #F01E39;
+	color: #131313;
+}
+
+.console {
+	width: 700px;
+	padding: 0 0 30px 0;
+	margin: 0 auto;
+	text-align: center;
+}
+.console h1 {
+	margin: 10px 0;
+	font-size: 50px;
+	color: #FAC223;
+}
+.console button {
+	color: #FAC223;
+	border: 1px solid #FAC223;
+	padding: 5px 10px;
+	background: none;
+	cursor: pointer;
+	font-family: 'Open Sans', sans-serif;
+	font-weight: 300;
+	font-size: 16px;
+	-moz-transition: .2s ease-in-out;
+  -o-transition: .2s ease-in-out;
+  -webkit-transition: .2s ease-in-out;
+  transition: .2s ease-in-out;
+}
+.console button:hover,
+.porg .console button#porg,
+.leiaorgana .console button#leiaorgana,
+.finn .console button#finn,
+.kyloren .console button#kyloren {
+	background: #FAC223;
+	color: #131313;
+}
+
+.stage {
+	width: 500px;
+	margin: 0 auto;
+}
+.main-stage {
+	width: 500px;
+	height: 500px;
+	position: relative;
+	background-size: cover;
+}
+
+.stage-quote {
+	position: relative;
+	margin-bottom: 100px;
+}
+.stage-quote h2 {
+	visibility: hidden;
+	opacity: 0;
+	position: absolute;
+	top: 0;
+	left: 0;
+	right: 0;
+	text-align: center;
+	text-transform: uppercase;
+	font-size: 26px;
+	color: #FAC223;
+	-moz-transition: 2s ease-in-out;
+	-o-transition: 2s ease-in-out;
+	-webkit-transition: 2s ease-in-out;
+	transition: 2s ease-in-out;
+}
+.porg .quote-porg,
+.leiaorgana .quote-leiaorgana,
+.finn .quote-finn,
+.kyloren .quote-kyloren {
+	visibility: visible;
+	opacity: 1;
+}
+
+.triangle {
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	-webkit-clip-path: polygon(0% 0%, 0% 0%, 0% 0%);
+}
+
+.t-1 {transition: 1.05s ease-out;}
+.t-2 {transition: 1.1s ease-out;}
+.t-3 {transition: 1.15s ease-out;}
+.t-4 {transition: 1.2s ease-out;}
+.t-5 {transition: 1.25s ease-out;}
+.t-6 {transition: 1.3s ease-out;}
+.t-7 {transition: 1.35s ease-out;}
+.t-8 {transition: 1.4s ease-out;}
+.t-9 {transition: 1.45s ease-out;}
+.t-10 {transition: 1.5s ease-out;}
+
+.t-11 {transition: 1.55s ease-out;}
+.t-12 {transition: 1.6s ease-out;}
+.t-13 {transition: 1.65s ease-out;}
+.t-14 {transition: 1.7s ease-out;}
+.t-15 {transition: 1.75s ease-out;}
+.t-16 {transition: 1.8s ease-out;}
+.t-17 {transition: 1.85s ease-out;}
+.t-18 {transition: 1.9s ease-out;}
+.t-19 {transition: 1.95s ease-out;}
+.t-20 {transition: 2s ease-out;}
+
+.t-21 {transition: 2.05s ease-out;}
+.t-22 {transition: 2.1s ease-out;}
+.t-23 {transition: 2.15s ease-out;}
+.t-24 {transition: 2.2s ease-out;}
+.t-25 {transition: 2.25s ease-out;}
+.t-26 {transition: 2.3s ease-out;}
+.t-27 {transition: 2.35s ease-out;}
+.t-28 {transition: 2.4s ease-out;}
+.t-29 {transition: 2.45s ease-out;}
+.t-30 {transition: 2.5s ease-out;}
+
+
+.glimmer .t-1,
+.glimmer .t-6,
+.glimmer .t-11,
+.glimmer .t-16,
+.glimmer .t-21,
+.glimmer .t-26 {animation: glimmer 1.5s;}
+
+.glimmer .t-2,
+.glimmer .t-7,
+.glimmer .t-12,
+.glimmer .t-17,
+.glimmer .t-22,
+.glimmer .t-27 {animation: glimmer 2s;}
+
+.glimmer .t-3,
+.glimmer .t-8,
+.glimmer .t-13,
+.glimmer .t-18,
+.glimmer .t-23,
+.glimmer .t-28 {animation: glimmer 2.5s;}
+
+.glimmer .t-4,
+.glimmer .t-9,
+.glimmer .t-14,
+.glimmer .t-19,
+.glimmer .t-24,
+.glimmer .t-29 {animation: glimmer 3s;}
+
+.glimmer .t-5,
+.glimmer .t-10,
+.glimmer .t-15,
+.glimmer .t-20,
+.glimmer .t-25,
+.glimmer .t-30 {animation: glimmer 3.5s;}
+
+
+@keyframes glimmer {
+	0% {opacity: 1;}
+	25% {opacity: 0.8;}
+	50% {opacity: 1;}
+	75% {opacity: 0.8;}
+}
+/* Kylo Ren */
+
+.kyloren .t-1 {-webkit-clip-path: polygon(50% 15%, 50% 35%, 66% 26%);}
+.kyloren .t-2 {-webkit-clip-path: polygon(50% 15%, 50% 35%, 34% 26%);}
+.kyloren .t-3 {-webkit-clip-path: polygon(77% 44%, 50% 35%, 66% 26%);}
+.kyloren .t-4 {-webkit-clip-path: polygon(23% 44%, 50% 35%, 34% 26%);}
+.kyloren .t-5 {-webkit-clip-path: polygon(77% 44%, 63% 39%, 72% 60%);}
+
+.kyloren .t-6 {-webkit-clip-path: polygon(23% 44%, 37% 39%, 28% 60%);}
+.kyloren .t-7 {-webkit-clip-path: polygon(77% 44%, 90% 65%, 72% 60%);}
+.kyloren .t-8 {-webkit-clip-path: polygon(23% 44%, 10% 65%, 28% 60%);}
+.kyloren .t-9 {-webkit-clip-path: polygon(72% 82%, 90% 65%, 72% 60%);}
+.kyloren .t-10 {-webkit-clip-path: polygon(28% 82%, 10% 65%, 28% 60%);}
+
+.kyloren .t-11 {-webkit-clip-path: polygon(50% 35%, 50% 40%, 63% 39%);}
+.kyloren .t-12 {-webkit-clip-path: polygon(50% 35%, 50% 40%, 37% 39%);}
+.kyloren .t-13 {-webkit-clip-path: polygon(50% 49%, 50% 40%, 63% 39%);}
+.kyloren .t-14 {-webkit-clip-path: polygon(50% 49%, 50% 40%, 37% 39%);}
+.kyloren .t-15 {-webkit-clip-path: polygon(50% 82%, 53% 51%, 67% 69%);}
+
+.kyloren .t-16 {-webkit-clip-path: polygon(50% 82%, 47% 51%, 33% 69%);}
+.kyloren .t-17 {-webkit-clip-path: polygon(50% 82%, 57% 83%, 61% 73%);}
+.kyloren .t-18 {-webkit-clip-path: polygon(50% 82%, 43% 83%, 39% 73%);}
+.kyloren .t-19 {-webkit-clip-path: polygon(50% 82%, 50% 51%, 53% 51%);}
+.kyloren .t-20 {-webkit-clip-path: polygon(50% 82%, 50% 51%, 47% 51%);}
+
+.kyloren .t-21 {-webkit-clip-path: polygon(62% 46%, 50% 49%, 61% 53%);}
+.kyloren .t-22 {-webkit-clip-path: polygon(38% 46%, 50% 49%, 39% 53%);}
+.kyloren .t-23 {-webkit-clip-path: polygon(63% 39%, 50% 49%, 62% 46%);}
+.kyloren .t-24 {-webkit-clip-path: polygon(37% 39%, 50% 49%, 38% 46%);}
+.kyloren .t-25 {-webkit-clip-path: polygon(63% 39%, 70% 56%, 61% 53%);}
+
+.kyloren .t-26 {-webkit-clip-path: polygon(37% 39%, 30% 56%, 39% 53%);}
+.kyloren .t-27 {-webkit-clip-path: polygon(64% 65%, 70% 56%, 56% 51%);}
+.kyloren .t-28 {-webkit-clip-path: polygon(36% 65%, 30% 56%, 44% 51%);}
+.kyloren .t-29 {-webkit-clip-path: polygon(53% 51%, 56% 51%, 64% 65%);}
+.kyloren .t-30 {-webkit-clip-path: polygon(47% 51%, 44% 51%, 36% 65%);}
+
+.kyloren .t-1,
+.kyloren .t-2,
+.kyloren .t-3,
+.kyloren .t-4,
+.kyloren .t-5,
+.kyloren .t-6,
+.kyloren .t-7,
+.kyloren .t-8,
+.kyloren .t-9,
+.kyloren .t-10,
+.kyloren .t-15,
+.kyloren .t-16,
+.kyloren .t-17,
+.kyloren .t-18 {background-color: #20201E;}
+
+.kyloren .t-13,
+.kyloren .t-14 {background-color: #9EA3A8;}
+
+.kyloren .t-11,
+.kyloren .t-12,
+.kyloren .t-19,
+.kyloren .t-20 {background-color: #2D2C2D;}
+
+.kyloren .t-21,
+.kyloren .t-22 {background-color: #131313;}
+
+.kyloren .t-23,
+.kyloren .t-24,
+.kyloren .t-25,
+.kyloren .t-26,
+.kyloren .t-27,
+.kyloren .t-28,
+.kyloren .t-29,
+.kyloren .t-30 {background-color: #C7C8C8;}
+</style>
+
+<div class="stage">
+
+			<div class="main-stage full-width">
+
+				<div class="triangle t-1"></div>
+				<div class="triangle t-2"></div>
+				<div class="triangle t-3"></div>
+				<div class="triangle t-4"></div>
+				<div class="triangle t-5"></div>
+
+				<div class="triangle t-6"></div>
+				<div class="triangle t-7"></div>
+				<div class="triangle t-8"></div>
+				<div class="triangle t-9"></div>
+				<div class="triangle t-10"></div>
+
+				<div class="triangle t-11"></div>
+				<div class="triangle t-12"></div>
+				<div class="triangle t-13"></div>
+				<div class="triangle t-14"></div>
+				<div class="triangle t-15"></div>
+
+				<div class="triangle t-16"></div>
+				<div class="triangle t-17"></div>
+				<div class="triangle t-18"></div>
+				<div class="triangle t-19"></div>
+				<div class="triangle t-20"></div>
+
+				<div class="triangle t-21"></div>
+				<div class="triangle t-22"></div>
+				<div class="triangle t-23"></div>
+				<div class="triangle t-24"></div>
+				<div class="triangle t-25"></div>
+
+				<div class="triangle t-26"></div>
+				<div class="triangle t-27"></div>
+				<div class="triangle t-28"></div>
+				<div class="triangle t-29"></div>
+				<div class="triangle t-30"></div>
+
+			</div>
+    </div>
