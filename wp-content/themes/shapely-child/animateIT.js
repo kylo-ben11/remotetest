50358c352a621c355c03d56c8fd12300ab068d85
kylo-ben11 <ben.knier@gmail.com>
2018-08-30 20:54:14 -0500

    finish up smoothing out homepage buttons and content

--- wp-content/themes/shapely-child/animateIT.js
+++ wp-content/themes/shapely-child/animateIT.js
@@ -8,7 +8,21 @@ function() {
   $(this).removeClass('animated rubberBand duration2  eds-on-hover');
 })
 
-//Animate Nav Buttons
+$('#shapely_home_parallax-3 a').hover(function() {
+    $(this).addClass('animated rubberBand duration2  eds-on-hover');
+},
+function() {
+  $(this).removeClass('animated rubberBand duration2  eds-on-hover');
+})
+
+$('#shapely_home_parallax-4 a').hover(function() {
+    $(this).addClass('animated rubberBand duration2  eds-on-hover');
+},
+function() {
+  $(this).removeClass('animated rubberBand duration2  eds-on-hover');
+})
+
+//Animate Nav and Contact Buttons
 $('.main-navigation .menu li a').hover(function() {
     $(this).addClass('animated jello duration1  eds-on-hover');
 },
@@ -30,5 +44,41 @@ $('#shapely_home_parallax-4 section img').addClass('animated bounceInDown durati
 $('#shapely_home_parallax-4 section .col-md-4').addClass('animated bounceInDown duration1 eds-on-scroll');
 $('#shapely_home_contact-2 input[type="submit"]').addClass('btn btn-lg btn-filled');
 
+//Smooth scrolling Buttons
+$("#shapely_home_parallax-2 a:nth-child(4)").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#shapely_home_contact-2").offset().top -103
+    }, 3500);
+});
+
+$("#shapely_home_parallax-2 a:nth-child(3)").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#shapely_home_parallax-5").offset().top -103
+    }, 3500);
+});
+
+$("#shapely_home_parallax-3 a").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#shapely_home_contact-2").offset().top -103
+    }, 2500);
+});
+
+$("#shapely_home_parallax-4 a").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#shapely_home_contact-2").offset().top -103
+    }, 2500);
+});
+
+$("#shapely_home_parallax-5 a").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#shapely_home_contact-2").offset().top -103
+    }, 1500);
+});
+
+$("#menu-item-91 a").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#shapely_home_contact-2").offset().top -103
+    }, 4000);
+});
 
 });
