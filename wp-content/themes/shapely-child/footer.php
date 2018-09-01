abfda8d1371239203cdd2a173c2299f2885130a2
kylo-ben11 <ben.knier@gmail.com>
2018-08-31 23:31:50 -0500

    multiple styling changes to line colors up correctly

--- wp-content/themes/shapely-child/footer.php
+++ wp-content/themes/shapely-child/footer.php
@@ -30,11 +30,10 @@
 					<?php echo wp_kses_post( get_theme_mod( 'shapely_footer_copyright' ) ); ?>
 				</div>
 				<div class="footer-credits">
-					<?php shapely_footer_info(); ?>
+					<p> &copy; <?= date("Y");?> Code Creative Web Development</p>
 				</div>
 			</div><!-- .site-info -->
 			<div class="col-sm-6 text-right">
-				<?php shapely_social_icons(); ?>
 			</div>
 		</div>
 	</div>
